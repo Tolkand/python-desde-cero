1316da0d83a6e9b9e3b6053ca327aab03c17ac01
Tolkand <andres@barriobrasil.cl>
2026-01-10 22:38:16 -0300

    ejercicioL2_1

---
+++ modulo-1-fundamentos/ejercicios/ejercicios_leccion_2_1.py
@@ -0,0 +1,12 @@
+numero1 = input("ingreseprimer número: ")
+numero2 = input("ingrese segundo número: ")
+
+suma = float(numero1) + float(numero2)
+resta = float(numero1) - float(numero2)
+multiplicacion = float(numero1) * float(numero2)
+division = float(numero1) / float(numero2)
+
+print("la suma es: " + str(suma))
+print("la resta es: " + str(resta))
+print("la multiplicación es: " + str(multiplicacion))
+print("la división es: " + str(division))
