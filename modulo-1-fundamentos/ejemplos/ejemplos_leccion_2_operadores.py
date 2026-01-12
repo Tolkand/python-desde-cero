0af85808cd4d0bd473a75a72204c2940852d4800
Tolkand <andres@barriobrasil.cl>
2026-01-12 13:56:20 -0300

    ejemplosL2

--- modulo-1-fundamentos/ejemplos/ejemplos_leccion_2_operadores.py
+++ modulo-1-fundamentos/ejemplos/ejemplos_leccion_2_operadores.py
@@ -1,3 +1,6 @@
+
+#ejemplo 1: Operadores de comparación
+
 edad = int(input("¿Cuál es tu edad?"))
 
 es_menor = edad < 18
@@ -5,3 +8,17 @@ es_adulto = edad >= 18
 
 print("¿Menor de edad?", es_menor)
 print("¿Mayor o igual a 18?", es_adulto)
+
+
+
+
+#ejemplo 2: Operadores lógicos
+
+edad = int(input("¿Cuál es tu edad? "))
+tiene_entrada = input("¿Tienes entrada? (s/n): ")
+
+tiene_entrada_bool = (tiene_entrada == "s")
+
+puede_entrar = (edad >= 18) and tiene_entrada_bool
+
+print("¿Puede entrar?", puede_entrar)
