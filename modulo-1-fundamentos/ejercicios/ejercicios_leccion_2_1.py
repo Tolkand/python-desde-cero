fc109126407f0eaa94775ce4a1326ff86c291cd3
Tolkand <andres@barriobrasil.cl>
2026-01-12 13:29:31 -0300

    EjerciciosL2

--- modulo-1-fundamentos/ejercicios/ejercicios_leccion_2_1.py
+++ modulo-1-fundamentos/ejercicios/ejercicios_leccion_2_1.py
@@ -1,3 +1,5 @@
+#ejercicio 1 leccion_2_operadores
+
 numero1 = input("ingreseprimer número: ")
 numero2 = input("ingrese segundo número: ")
 
@@ -10,3 +12,14 @@ print("la suma es: " + str(suma))
 print("la resta es: " + str(resta))
 print("la multiplicación es: " + str(multiplicacion))
 print("la división es: " + str(division))
+
+#ejercicio 2 leccion_2_operadores
+
+persona1 = int(input("Ingresa la edad de la persona 1: "))
+persona2 = int(input("Ingresa la edad de la persona 2: "))
+
+print("la persona1 es mayor", persona1 > persona2)
+print("la persona2 es mayor", persona2 >  persona1)
+print("las edades son iguales", persona1 == persona2)
+print("la persona1 es mayor o igual a la persona2", persona1 >= persona2)
+     
